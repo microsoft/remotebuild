0f97d1faa836886b13b0424919e4f69d23e2b280
Jimmy Thomson <jithomso@microsoft.com>
2015-05-26 10:16:41 -0700

    Added copyback.cmd batch file which will copy the localized resources back to the taco git sources

---
+++ loc/Localization/HexCordovaTools/taco/Loc/copyback.cmd
@@ -0,0 +1,12 @@
+set LOCDROP=..\locdrop\bin
+setlocal ENABLEDELAYEDEXPANSION
+
+for /f %%l in ('dir /b %LOCDROP%') do (
+    set al=%%l
+    set bl=!al:~0,2!
+    if "!bl!" == "zh" (set cl=!al!) else (set cl=!bl!)
+
+    for /f %%p in ('dir /b %LOCDROP%\%%l') do (
+     	robocopy %LOCDROP%\%%l\%%p\resources\en\ %TACO_ROOT%\src\%%p\resources\!cl!\
+    )
+)
