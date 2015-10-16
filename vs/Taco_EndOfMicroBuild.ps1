94cd44dafcf9fba370b775334c5ca6c7b9a7127e
Nisheet Jain <nisheet.jain@microsoft.com>
2015-10-15 22:51:10 -0700

    Fixing the path for Tacosrc

--- vs/Taco_EndOfMicroBuild.ps1
+++ vs/Taco_EndOfMicroBuild.ps1
@@ -1,7 +1,10 @@
-$TacoRoot = "$env:TF_BUILD_SOURCESDIRECTORY\src";
+$TacoRoot = "$env:TF_BUILD_SOURCESDIRECTORY\";
 $GulpPath = "$TacoRoot\node_modules\gulp\bin\gulp.js";
 $TacoSrc = "$TacoRoot\src";
 
+echo "Running unprep to clean up installed dev depdencies";
 pushd $TacoSrc
 node $GulpPath unprep 
 popd
+
+echo "Finished unprep";
