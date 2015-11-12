5bbb551d10143fbcc375cc69f28fbe542bb09200
Nisheet Jain <nisheet.jain@microsoft.com>
2015-11-12 01:03:58 -0800

    Updating Microbuild with new TACO build instructions

--- vs/Taco_EndOfMicroBuild.ps1
+++ vs/Taco_EndOfMicroBuild.ps1
@@ -1,10 +1,8 @@
 $TacoRoot = "$env:TF_BUILD_SOURCESDIRECTORY\";
-$GulpPath = "$TacoRoot\node_modules\gulp\bin\gulp.js";
-$TacoSrc = "$TacoRoot\src";
 
-echo "Running unprep to clean up installed dev depdencies";
-pushd $TacoSrc
-node $GulpPath unprep 
+echo "Running clean to clean up installed dev depdencies";
+pushd $TacoRoot
+npm run clean
 popd
 
-echo "Finished unprep";
+echo "Finished cleaning repository";
