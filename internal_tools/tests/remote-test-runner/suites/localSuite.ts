89f928950359c110ba47634fea53065c47c862b3
Guillaume Jenkins <gujenkin@microsoft.com>
2015-11-23 12:50:09 -0800

    Implemented local suite

--- internal_tools/tests/remote-test-runner/suites/localSuite.ts
+++ internal_tools/tests/remote-test-runner/suites/localSuite.ts
@@ -1,9 +1,13 @@
+/// <reference path="../typings/mocha.d.ts" />
 /// <reference path="../typings/node.d.ts" />
 /// <reference path="../typings/q.d.ts" />
 /// <reference path="../typings/remoteTestRunnerInterfaces.d.ts" />
 
 "use strict";
 
+import path = require("path");
+
+import mocha = require("mocha");
 import Q = require("q");
 
 import AbstractSuite = require("./abstractSuite");
@@ -16,7 +20,37 @@ class LocalSuite extends AbstractSuite {
     }
 
     protected launch(): Q.Promise<any> {
-        throw new Error("Not implemented");
+        var mochaRunner: Mocha;
+        var deferred: Q.Deferred<any> = Q.defer<any>();
+
+        // Create the mocha runner
+        if (this.mochaReporter) {
+            mochaRunner = new mocha({
+                reporter: this.mochaReporter
+            });
+        } else {
+            mochaRunner = new mocha();
+        }
+
+        // Add the test files to the mocha runner
+        this.testFiles.forEach((fileRelativePath: string) => {
+            // The test files are relative to the test package root, so path.join() them with the package root when adding them to mocha
+            mochaRunner.addFile(path.join(this.testPackage, fileRelativePath));
+        });
+
+        // Run the tests
+        mochaRunner.run((failures: number) => {
+            if (failures) {
+                // If we had at least one failure, reject the promise
+                deferred.reject(new Error("At least one test failed"));
+            } else {
+                // All tests passed
+                deferred.resolve({});
+            }
+        });
+
+        // Return the result
+        return deferred.promise;
     }
 }
 
